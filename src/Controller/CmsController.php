d06171e1204e71028d3e748897f941d730767f5b
Bartosz Juszczyk <b.juszczyk@bjuszczyk.pl>
2025-07-11 01:35:18 +0200

    Basic dashboard view

---
+++ src/Controller/CmsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CmsController extends AbstractController
+{
+    #[Route('/', name: 'app_cms_home', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('dashboard.html.twig', [
+            'app_title' => $this->getParameter('app_title'),
+            'page_title' => $this->getParameter('app_title').' – My Tickets',
+            'board_title' => 'My tickets',
+        ]);
+    }
+}
